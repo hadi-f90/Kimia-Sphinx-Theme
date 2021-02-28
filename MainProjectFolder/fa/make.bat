84119a78a9842b7a7242036c5f8c52383265cabc
hadi-f90 <62212402+hadi-f90@users.noreply.github.com>
2021-02-28 08:15:41 +0330

    Reverting date changes in make.bat
    Removing folder date-time  structures

--- MainProjectFolder/fa/make.bat
+++ MainProjectFolder/fa/make.bat
@@ -6,7 +6,6 @@ if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
 set BUILDDIR=../../../builds
-set DATETIME=%date%-%time%
 set ALLSPHINXOPTS=-d %BUILDDIR%/doctrees_fa %SPHINXOPTS% .
 set I18NSPHINXOPTS=%SPHINXOPTS% .
 if NOT "%PAPER%" == "" (
@@ -64,31 +63,31 @@ if errorlevel 9009 (
 )
 
 if "%1" == "html" (
-	%SPHINXBUILD% -b html %ALLSPHINXOPTS% %BUILDDIR%/html/%DATETIME%
+	%SPHINXBUILD% -b html %ALLSPHINXOPTS% %BUILDDIR%/html/
 	if errorlevel 1 exit /b 1
 	echo.
-	echo.Build finished. The HTML pages are in %BUILDDIR%/html/%DATETIME%.
+	echo.Build finished. The HTML pages are in %BUILDDIR%/html/.
 	goto end
 )
 
 if "%1" == "dirhtml" (
-	%SPHINXBUILD% -b dirhtml %ALLSPHINXOPTS% %BUILDDIR%/dirhtml/%DATETIME%
+	%SPHINXBUILD% -b dirhtml %ALLSPHINXOPTS% %BUILDDIR%/dirhtml/
 	if errorlevel 1 exit /b 1
 	echo.
-	echo.Build finished. The HTML pages are in %BUILDDIR%/dirhtml/%DATETIME%.
+	echo.Build finished. The HTML pages are in %BUILDDIR%/dirhtml/.
 	goto end
 )
 
 if "%1" == "singlehtml" (
-	%SPHINXBUILD% -b singlehtml %ALLSPHINXOPTS% %BUILDDIR%/singlehtml/%DATETIME%
+	%SPHINXBUILD% -b singlehtml %ALLSPHINXOPTS% %BUILDDIR%/singlehtml/
 	if errorlevel 1 exit /b 1
 	echo.
-	echo.Build finished. The HTML pages are in %BUILDDIR%/singlehtml/%DATETIME%.
+	echo.Build finished. The HTML pages are in %BUILDDIR%/singlehtml/.
 	goto end
 )
 
 if "%1" == "pickle" (
-	%SPHINXBUILD% -b pickle %ALLSPHINXOPTS% %BUILDDIR%/pickle/%DATETIME%
+	%SPHINXBUILD% -b pickle %ALLSPHINXOPTS% %BUILDDIR%/pickle/
 	if errorlevel 1 exit /b 1
 	echo.
 	echo.Build finished; now you can process the pickle files.
@@ -96,7 +95,7 @@ if "%1" == "pickle" (
 )
 
 if "%1" == "json" (
-	%SPHINXBUILD% -b json %ALLSPHINXOPTS% %BUILDDIR%/json/%DATETIME%
+	%SPHINXBUILD% -b json %ALLSPHINXOPTS% %BUILDDIR%/json/
 	if errorlevel 1 exit /b 1
 	echo.
 	echo.Build finished; now you can process the JSON files.
@@ -104,20 +103,20 @@ if "%1" == "json" (
 )
 
 if "%1" == "htmlhelp" (
-	%SPHINXBUILD% -b htmlhelp %ALLSPHINXOPTS% %BUILDDIR%/htmlhelp/%DATETIME%
+	%SPHINXBUILD% -b htmlhelp %ALLSPHINXOPTS% %BUILDDIR%/htmlhelp/
 	if errorlevel 1 exit /b 1
 	echo.
 	echo.Build finished; now you can run HTML Help Workshop with the ^
-.hhp project file in %BUILDDIR%/%DATETIME% htmlhelp.
+.hhp project file in %BUILDDIR%/ htmlhelp.
 	goto end
 )
 
 if "%1" == "qthelp" (
-	%SPHINXBUILD% -b qthelp %ALLSPHINXOPTS% %BUILDDIR%/qthelp/%DATETIME%
+	%SPHINXBUILD% -b qthelp %ALLSPHINXOPTS% %BUILDDIR%/qthelp/
 	if errorlevel 1 exit /b 1
 	echo.
 	echo.Build finished; now you can run "qcollectiongenerator" with the ^
-.qhcp project file in %BUILDDIR%/%DATETIME% qthelp, like this:
+.qhcp project file in %BUILDDIR%/ qthelp, like this:
 	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\ReeborgsWorld.qhcp
 	echo.To view the help file:
 	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\ReeborgsWorld.ghc
@@ -125,7 +124,7 @@ if "%1" == "qthelp" (
 )
 
 if "%1" == "devhelp" (
-	%SPHINXBUILD% -b devhelp %ALLSPHINXOPTS% %BUILDDIR%/devhelp/%DATETIME%
+	%SPHINXBUILD% -b devhelp %ALLSPHINXOPTS% %BUILDDIR%/devhelp/
 	if errorlevel 1 exit /b 1
 	echo.
 	echo.Build finished.
@@ -133,96 +132,96 @@ if "%1" == "devhelp" (
 )
 
 if "%1" == "epub" (
-	%SPHINXBUILD% -b epub %ALLSPHINXOPTS% %BUILDDIR%/epub/%DATETIME%
+	%SPHINXBUILD% -b epub %ALLSPHINXOPTS% %BUILDDIR%/epub/
 	if errorlevel 1 exit /b 1
 	echo.
-	echo.Build finished. The epub file is in %BUILDDIR%/epub/%DATETIME%.
+	echo.Build finished. The epub file is in %BUILDDIR%/epub/.
 	goto end
 )
 
 if "%1" == "latex" (
-	%SPHINXBUILD% -b latex %ALLSPHINXOPTS% %BUILDDIR%/latex/%DATETIME%
+	%SPHINXBUILD% -b latex %ALLSPHINXOPTS% %BUILDDIR%/latex/
 	if errorlevel 1 exit /b 1
 	echo.
-	echo.Build finished; the LaTeX files are in %BUILDDIR%/latex/%DATETIME%.
+	echo.Build finished; the LaTeX files are in %BUILDDIR%/latex/.
 	goto end
 )
 
 if "%1" == "latexpdf" (
-	%SPHINXBUILD% -b latex %ALLSPHINXOPTS% %BUILDDIR%/latex/%DATETIME%
-	cd %BUILDDIR%/latex/%DATETIME%/
+	%SPHINXBUILD% -b latex %ALLSPHINXOPTS% %BUILDDIR%/latex/
+	cd %BUILDDIR%/latex/
 	make all-pdf
 	cd %BUILDDIR%/..
 	echo.
-	echo.Build finished; the PDF files are in %BUILDDIR%/latex/%DATETIME%.
+	echo.Build finished; the PDF files are in %BUILDDIR%/latex/.
 	goto end
 )
 
 if "%1" == "latexpdfja" (
-	%SPHINXBUILD% -b latex %ALLSPHINXOPTS% %BUILDDIR%/latex/%DATETIME%
-	cd %BUILDDIR%/latex/%DATETIME%
+	%SPHINXBUILD% -b latex %ALLSPHINXOPTS% %BUILDDIR%/latex/
+	cd %BUILDDIR%/latex/
 	make all-pdf-ja
 	cd %BUILDDIR%/..
 	echo.
-	echo.Build finished; the PDF files are in %BUILDDIR%/latex/%DATETIME%.
+	echo.Build finished; the PDF files are in %BUILDDIR%/latex/.
 	goto end
 )
 
 if "%1" == "text" (
-	%SPHINXBUILD% -b text %ALLSPHINXOPTS% %BUILDDIR%/text/%DATETIME%
+	%SPHINXBUILD% -b text %ALLSPHINXOPTS% %BUILDDIR%/text/
 	if errorlevel 1 exit /b 1
 	echo.
-	echo.Build finished. The text files are in %BUILDDIR%/text/%DATETIME%.
+	echo.Build finished. The text files are in %BUILDDIR%/text/.
 	goto end
 )
 
 if "%1" == "man" (
-	%SPHINXBUILD% -b man %ALLSPHINXOPTS% %BUILDDIR%/man/%DATETIME%
+	%SPHINXBUILD% -b man %ALLSPHINXOPTS% %BUILDDIR%/man/
 	if errorlevel 1 exit /b 1
 	echo.
-	echo.Build finished. The manual pages are in %BUILDDIR%/man/%DATETIME%.
+	echo.Build finished. The manual pages are in %BUILDDIR%/man/.
 	goto end
 )
 
 if "%1" == "texinfo" (
-	%SPHINXBUILD% -b texinfo %ALLSPHINXOPTS% %BUILDDIR%/texinfo/%DATETIME%
+	%SPHINXBUILD% -b texinfo %ALLSPHINXOPTS% %BUILDDIR%/texinfo/
 	if errorlevel 1 exit /b 1
 	echo.
-	echo.Build finished. The Texinfo files are in %BUILDDIR%/texinfo/%DATETIME%.
+	echo.Build finished. The Texinfo files are in %BUILDDIR%/texinfo/.
 	goto end
 )
 
 if "%1" == "gettext" (
-	%SPHINXBUILD% -b gettext %I18NSPHINXOPTS% %BUILDDIR%/locale/%DATETIME%
+	%SPHINXBUILD% -b gettext %I18NSPHINXOPTS% %BUILDDIR%/locale/
 	if errorlevel 1 exit /b 1
 	echo.
-	echo.Build finished. The message catalogs are in %BUILDDIR%/locale/%DATETIME%.
+	echo.Build finished. The message catalogs are in %BUILDDIR%/locale/.
 	goto end
 )
 
 if "%1" == "changes" (
-	%SPHINXBUILD% -b changes %ALLSPHINXOPTS% %BUILDDIR%/changes/%DATETIME%
+	%SPHINXBUILD% -b changes %ALLSPHINXOPTS% %BUILDDIR%/changes/
 	if errorlevel 1 exit /b 1
 	echo.
-	echo.The overview file is in %BUILDDIR%/changes/%DATETIME%.
+	echo.The overview file is in %BUILDDIR%/changes/.
 	goto end
 )
 
 if "%1" == "linkcheck" (
-	%SPHINXBUILD% -b linkcheck %ALLSPHINXOPTS% %BUILDDIR%/linkcheck/%DATETIME%
+	%SPHINXBUILD% -b linkcheck %ALLSPHINXOPTS% %BUILDDIR%/linkcheck/
 	if errorlevel 1 exit /b 1
 	echo.
 	echo.Link check complete; look for any errors in the above output ^
-or in %BUILDDIR%/linkcheck/%DATETIME%/output.txt.
+or in %BUILDDIR%/linkcheck/output.txt.
 	goto end
 )
 
 if "%1" == "doctest" (
-	%SPHINXBUILD% -b doctest %ALLSPHINXOPTS% %BUILDDIR%/doctest/%DATETIME%
+	%SPHINXBUILD% -b doctest %ALLSPHINXOPTS% %BUILDDIR%/doctest/
 	if errorlevel 1 exit /b 1
 	echo.
 	echo.Testing of doctests in the sources finished, look at the ^
-results in %BUILDDIR%/doctest/%DATETIME%/output.txt.
+results in %BUILDDIR%/doctest/output.txt.
 	goto end
 )
 
